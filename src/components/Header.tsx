9963814dbd3e0e867d9e3e15fbcc8651aaf3166a
vivaldev_ <149679225+valvira-gh@users.noreply.github.com>
2024-05-14 01:52:47 +0300

    Initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header: React.FC = () => {
+  return (
+    <header className="flex flex-col items-center mt-2">
+      <h1 className="text-6xl text-teal-800 font-bold font-sans">Pet Data</h1>
+    </header>
+  );
+};
+
+export default Header;
